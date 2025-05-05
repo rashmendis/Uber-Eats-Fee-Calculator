b2e20021744afdbdc7ab6e09a1010ae85c19ac83
Rash Mendis <rashmimendis7@gmail.com>
2025-05-05 16:19:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <table>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <div className="w-full">
    >       <_c2>
    >       <_c8 className="px-0 pb-2 ...">
    >         <div ref={null} className="p-6 px-0 p...">
    >           <_c className="h-[300px] ...">
    >             <ScrollArea ref={null} className="relative o...">
    >               <ScrollAreaProvider scope={undefined} type="hover" dir="ltr" scrollHideDelay={600} scrollArea={null} ...>
    >                 <Primitive.div dir="ltr" className="relative o..." ref={function} style={{position:"...", ...}}>
    >                   <div dir="ltr" className="relative o..." style={{position:"...", ...}} ref={function}>
    >                     <ScrollAreaViewport className="h-full w-f...">
    >                       <style>
    >                       <Primitive.div data-radix-scroll-area-viewport="" className="h-full w-f..." ref={function} ...>
    >                         <div data-radix-scroll-area-viewport="" className="h-full w-f..." style={{...}} ref={function}>
    >                           <div ref={function bound dispatchSetState} style={{minWidth:"...", ...}}>
    >                             <_c className="min-w-full...">
    >                               <div className="relative w...">
    > >                               <table ref={null} className="w-full caption-bottom text-sm min-w-full relative">
    > >                                 {" "}
    >                                   ...
    >                     ...
    >
    >
    > src/components/ui/table.tsx (11:5) @ _c
    >
    >    9 | >(({ className, ...props }, ref) => (
    >   10 |   <div className="relative w-full overflow-auto">
    > > 11 |     <table
    >      |     ^
    >   12 |       ref={ref}
    >   13 |       className={cn("w-full caption-bottom text-sm", className)}
    >   14 |       {...props} // Children are passed down here
    >
    > Call Stack
    > 19
    >
    > Show 15 ignore-listed frame(s)
    > table
    > <anonymous> (0:0)
    > _c
    > src/components/ui/table.tsx (11:5)
    > HistoryView
    > src/components/history-view.tsx (144:13)
    > Home
    > src/app/page.tsx (39:15)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,13 +6,15 @@ import { cn } from "@/lib/utils"
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
-      {...props} // Children are passed down here
-    />
+      {...props} // Spread remaining props
+    >
+      {children} {/* Render children explicitly inside the table */}
+    </table>
   </div>
 ))
 Table.displayName = "Table"
@@ -20,27 +22,31 @@ Table.displayName = "Table"
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
+  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props}>
+    {children} {/* Render children explicitly inside the thead */}
+  </thead>
 ))
 TableHeader.displayName = "TableHeader"
 
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <tbody
     ref={ref}
     className={cn("[&_tr:last-child]:border-0", className)}
     {...props}
-  />
+  >
+     {children} {/* Render children explicitly inside the tbody */}
+  </tbody>
 ))
 TableBody.displayName = "TableBody"
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <tfoot
     ref={ref}
     className={cn(
@@ -48,61 +54,71 @@ const TableFooter = React.forwardRef<
       className
     )}
     {...props}
-  />
+  >
+     {children} {/* Render children explicitly inside the tfoot */}
+  </tfoot>
 ))
 TableFooter.displayName = "TableFooter"
 
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <tr
     ref={ref}
     className={cn(
       "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
       className
     )}
-    {...props} // Children are passed down here
-  />
+    {...props}
+  >
+    {children} {/* Render children explicitly inside the tr */}
+  </tr>
 ))
 TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <th
     ref={ref}
     className={cn(
       "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
-    {...props} // Children are passed down here
-  />
+    {...props}
+  >
+    {children} {/* Render children explicitly inside the th */}
+  </th>
 ))
 TableHead.displayName = "TableHead"
 
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <td
     ref={ref}
     className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
-    {...props} // Children are passed down here
-  />
+    {...props}
+  >
+    {children} {/* Render children explicitly inside the td */}
+  </td>
 ))
 TableCell.displayName = "TableCell"
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
->(({ className, ...props }, ref) => (
+>(({ className, children, ...props }, ref) => ( // Explicitly destructure children
   <caption
     ref={ref}
     className={cn("mt-4 text-sm text-muted-foreground", className)}
-    {...props} // Children are passed down here
-  />
+    {...props}
+  >
+    {children} {/* Render children explicitly inside the caption */}
+  </caption>
 ))
 TableCaption.displayName = "TableCaption"
 
